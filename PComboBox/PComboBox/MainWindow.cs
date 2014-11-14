487b65ab3d4295e32eff4fb17a25ab4995e2c8d3
luisvelasco <luis.velasco.serpis@gmail.com>
2014-11-14 12:15:53 +0100

    PComboBox: añadidos ComboBoxHelper y ComboBoxExtensions

--- PComboBox/PComboBox/MainWindow.cs
+++ PComboBox/PComboBox/MainWindow.cs
@@ -1,6 +1,7 @@
 using Gtk;
 using System;
-using System.Collections.Generic;
+
+using SerpisAd;
 
 public partial class MainWindow: Gtk.Window
 {	
@@ -8,44 +9,10 @@ public partial class MainWindow: Gtk.Window
 	{
 		Build ();
 
-		List<Categoria> categorias = new List<Categoria> ();
-		categorias.Add (new Categoria (1, "Uno"));
-		categorias.Add (new Categoria (2, "Dos"));
-		categorias.Add (new Categoria (3, "Tres"));
-		categorias.Add (new Categoria (4, "Cuatro"));
-
-		int categoriaId = -1;
-
-		CellRendererText cellRendererText = new CellRendererText ();
-		comboBox.PackStart (cellRendererText, false);
-		comboBox.AddAttribute (cellRendererText, "text", 1);
-
-		ListStore listStore = new ListStore (typeof(int), typeof(string));
-		TreeIter initialTreeIter = listStore.AppendValues (0, "<sin asignar>");
-
-		foreach (Categoria categoria in categorias)
-			listStore.AppendValues (categoria.Id, categoria.Nombre);
-
-		comboBox.Model = listStore;
-
-		comboBox.SetActiveIter (initialTreeIter);
-
-		TreeIter currentTreeIter;
-		listStore.GetIterFirst (out currentTreeIter);
-		do {
-			if (categoriaId.Equals(listStore.GetValue(currentTreeIter, 0)) ){
-				comboBox.SetActiveIter (currentTreeIter);
-				break;
-			}
-		} while (listStore.IterNext (ref currentTreeIter));
-
+		new ComboBoxHelper (comboBox, (ulong)7, "select id, nombre from categoria");
 
 		propertiesAction.Activated += delegate {
-			TreeIter treeIter;
-			bool activeIter = comboBox.GetActiveIter(out treeIter);
-			object id = activeIter ? listStore.GetValue(treeIter, 0) : 0;
-			Console.WriteLine("id={0}", id);
-
+			Console.WriteLine("id={0}", comboBox.GetId());
 		};
 
 	}
